5ba37222f51b8cd51e37685c94b561ac9e403039
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2025-05-19 10:33:50 +0530

    Create floyd.c

---
+++ Lab6/DP floyd/floyd.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#define INF 99999
+#define V 4
+
+void floydWarshall(int graph[][V]) {
+    int dist[V][V], i, j, k;
+
+
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+
+    printf("Shortest path matrix:\n");
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+
+    int graph[V][V] = {
+        {0, INF, 3, INF},
+        {2, 0, INF, INF},
+        {INF, 7, 0, 1},
+        {6, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+
+    return 0;
+}
